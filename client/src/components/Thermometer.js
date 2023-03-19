8c9364b6d3389b7e4199fe3db52daec3bea7038b
DeveshKrishan <deveshkrishan2003@gmail.com>
2023-03-19 15:22:35 -0700

    finished with therometer and trackpage

--- client/src/components/Thermometer.js
+++ client/src/components/Thermometer.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, {useRef} from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const Thermometer = (props) => {
@@ -6,11 +6,19 @@ const Thermometer = (props) => {
     Init var and assign to passed in props var
     useState vs useRef
     */
+    // const intervalRef = useRef(0);
+
+    let percentStr = (props.percent*100).toFixed() + '%';    ;
+
+    console.log(percentStr, props.percent);
+
+
+    const initName = useRef(props.name);
+    const initPercent = useRef(percentStr);
 
     /* Utilize ProgressBar to build color may not work*/
     return (
-        <>
-        </>
+        <ProgressBar label = {initName.current + ": " + initPercent.current + "%"}now={initPercent.current} variant="info"/>
     )
 };
 export default Thermometer;
--- client/src/components/TrackPage.js
+++ client/src/components/TrackPage.js
@@ -21,7 +21,15 @@ const TrackPage = (props) => {
     , [])
 
     // Use link to route page, see TrackList for ref
+
+    const trackClicked = (id) =>{
+        props.setId(id)
+    }
+
     // check if track and audioFeature exists, see AlbumPage for ref
+
+
+
     // Use .map to loop through array, see CarouselComponent for ref
         // Ensure that in the .map you return html
     // return only one big parent tag
@@ -29,8 +37,50 @@ const TrackPage = (props) => {
     // Don't worry about css just get the information on the page
     return (
         <div>
-            <p>{trackId.current}</p>
+            { track ?
+            <div class="row">
+                <div class="col-6">
+                    
+                    <h1>{track.name}</h1>
+                    <p>
+                        Track on <Link to="/album" onClick={() => trackClicked(track.album.id)}>{track.album.name}</Link>
+
+                        {/* Track on <a href="http://localhost:4200/album/{{track.album.id}}">{track.album.name}</a> */}
+                    </p>
+                    <div>
+                        {
+                            track.artists.map((value, index) => {
+                                return (
+                                    <p>
+                                    Artist: <Link to="/artist" onClick={() => trackClicked(value.id)}>{value.name}</Link>
+                                </p>
+                                )
+                            })
+                        }
+                    </div>
+                    <p>
+                        Duration: {track.durationStr}
+                    </p>
+                    <p>
+                        <a class="btn btn-light" role="button" href = "{{track.url}}" target="_blank">Open {track.name} on Spotify</a>
+                    </p>
+                </div>
+                <div class="col-6">
+                    {
+                        audioFeatures.map((value) => {
+                            console.log(value)
+                            return <Thermometer name={value.name} percent= {value.percent}></Thermometer>
+                        })
+                    }
+                </div>
+            </div>
+            :null 
+            }
         </div>
+        
     )
+
+    
+
 };
 export default TrackPage;
